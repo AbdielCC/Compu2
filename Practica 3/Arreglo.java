a2d2c67c141ddd0662dcd9823582ff286b7b0232
Cabello Cardenas Ulises Abdiel <105956768+AbdielCC@users.noreply.github.com>
2024-09-09 12:53:52 -0600

    Add files via upload

--- Practica 3/Arreglo.java
+++ Practica 3/Arreglo.java
@@ -2,7 +2,7 @@ import java.util.NoSuchElementException;
 
 /**
  * Clase que implementa la interfaz {@code Coleccion} utilizando un arreglo.
- * Esta clase proporciona métodos para manejar arreglos unidimensionales y multidimensionales,
+ * Esta clase proporciona metodos para manejar arreglos unidimensionales y multidimensionales,
  * incluyendo soporte para Vectores de Iliffe en arreglos multidimensionales.
  *
  * @param <T> El tipo de elementos que contiene el arreglo.
@@ -38,10 +38,9 @@ public class Arreglo<T> implements Coleccion<T> {
     public Arreglo(int... dimensiones) {
         if (dimensiones.length == 0 || dimensiones[0] <= 0) {
             throw new IllegalArgumentException("Dimensiones inválidas");
-        } else {
-            this.dimensiones = dimensiones;
-            iliffeVector = crearIliffeVector(dimensiones, 0);
         }
+        this.dimensiones = dimensiones;
+        iliffeVector = crearIliffeVector(dimensiones, 0);
     }
 
     /**
@@ -52,68 +51,23 @@ public class Arreglo<T> implements Coleccion<T> {
      * @return Un vector de objetos que representa la estructura del arreglo multidimensional.
      */
     private Object[] crearIliffeVector(int[] dims, int nivel) {
-        int size = dims[nivel];
-        Object[] vector = new Object[size];
+        int dim = dims[nivel];
+        Object[] vector = new Object[dim];
         if (nivel == dims.length - 1) {
-            for (int i = 0; i < size; i++) {
-                vector[i] = null; // Inicialización
-            }
-        } else {
-            for (int i = 0; i < size; i++) {
-                vector[i] = crearIliffeVector(dims, nivel + 1);
-            }
+            return vector;
+        }
+        for (int i = 0; i < dim; i++) {
+            vector[i] = crearIliffeVector(dims, nivel + 1);
         }
         return vector;
     }
 
     /**
-     * Método para establecer un valor en una posición específica del arreglo multidimensional.
+     * Obtiene el valor en la posicion especificada del arreglo multidimensional.
      *
-     * @param valor El valor a establecer.
-     * @param indices Los índices para acceder a la posición en el arreglo.
-     * @throws IndexOutOfBoundsException Si el número de índices es incorrecto o está fuera de los límites.
-     */
-    public void establecer(T valor, int... indices) {
-        setArreglo(valor, indices);
-    }
-
-    /**
-     * Establece un valor en la posición especificada del arreglo multidimensional.
-     *
-     * @param valor El valor a establecer.
-     * @param indices Los índices para acceder a la posición en el arreglo.
-     * @throws IndexOutOfBoundsException Si el número de índices es incorrecto o fuera de los límites.
-     */
-    @SuppressWarnings("unchecked")
-    public void setArreglo(T valor, int... indices) {
-        if (indices.length != dimensiones.length) {
-            throw new IndexOutOfBoundsException("Número incorrecto de índices");
-        }
-        Object actual = iliffeVector;
-        for (int i = 0; i < indices.length - 1; i++) {
-            actual = ((Object[]) actual)[indices[i]];
-        }
-        ((Object[]) actual)[indices[indices.length - 1]] = valor;
-    }
-
-    /**
-     * Obtiene el valor en la posición especificada del arreglo multidimensional.
-     *
-     * @param indices Los índices para acceder al valor en el arreglo.
-     * @return El valor en la posición especificada.
-     * @throws IndexOutOfBoundsException Si el número de índices es incorrecto o fuera de los límites.
-     */
-    @SuppressWarnings("unchecked")
-    public T obtener(int... indices) {
-        return getArreglo(indices);
-    }
-
-    /**
-     * Obtiene el valor en la posición especificada del arreglo multidimensional.
-     *
-     * @param indices Los índices para acceder al valor en el arreglo.
-     * @return El valor en la posición especificada.
-     * @throws IndexOutOfBoundsException Si el número de índices es incorrecto o fuera de los límites.
+     * @param indices Los indices para acceder al valor en el arreglo.
+     * @return El valor en la posicion especificada.
+     * @throws IndexOutOfBoundsException Si el numero de indices es incorrecto o fuera de los limites.
      */
     @SuppressWarnings("unchecked")
     public T getArreglo(int... indices) {
@@ -122,13 +76,42 @@ public class Arreglo<T> implements Coleccion<T> {
         }
         Object actual = iliffeVector;
         for (int i = 0; i < indices.length; i++) {
+            if (indices[i] < 0 || indices[i] >= ((Object[]) actual).length) {
+                throw new IndexOutOfBoundsException("Indice fuera de los limites");
+            }
             actual = ((Object[]) actual)[indices[i]];
         }
         return (T) actual;
     }
 
-    // Metodos para manejo de arreglos unidimensionales y otros metodos de la clase
+    /**
+     * Establece un valor en la posicion especificada del arreglo multidimensional.
+     *
+     * @param valor El valor a setArreglo.
+     * @param indices Los indices para acceder a la posicion en el arreglo.
+     * @throws IndexOutOfBoundsException Si el numero de indices es incorrecto o fuera de los limites.
+     */
+    @SuppressWarnings("unchecked")
+    public void setArreglo(T valor, int... indices) {
+        if (indices.length != dimensiones.length) {
+            throw new IndexOutOfBoundsException("Numero incorrecto de indices");
+        }
+        Object actual = iliffeVector;
+        for (int i = 0; i < indices.length - 1; i++) {
+            if (indices[i] < 0 || indices[i] >= ((Object[]) actual).length) {
+                throw new IndexOutOfBoundsException("Indice fuera de los limites");
+            }
+            actual = ((Object[]) actual)[indices[i]];
+        }
+        ((Object[]) actual)[indices[indices.length - 1]] = valor;
+    }
 
+    /**
+     * Agrega un nuevo elemento al final del arreglo unidimensional.
+     *
+     * @param elemento El elemento a agregar.
+     * @throws IllegalStateException Si el arreglo esta lleno.
+     */
     @Override
     public void agregar(T elemento) {
         if (longitud >= elementos.length) {
@@ -137,24 +120,43 @@ public class Arreglo<T> implements Coleccion<T> {
         elementos[longitud++] = elemento;
     }
 
+    /**
+     * Elimina y retorna el ultimo elemento del arreglo unidimensional.
+     *
+     * @return El elemento eliminado.
+     * @throws NoSuchElementException Si el arreglo esta vacio.
+     */
     @Override
     public T eliminar() {
         if (longitud == 0) {
             throw new NoSuchElementException("El arreglo esta vacio");
         }
         T elemento = elementos[--longitud];
-        elementos[longitud] = null;
+        elementos[longitud] = null;  // Liberar referencia
         return elemento;
     }
 
+    /**
+     * Obtiene el elemento en la posicion especificada del arreglo unidimensional.
+     *
+     * @param indice El indice del elemento a getArreglo.
+     * @return El elemento en la posicion especificada.
+     * @throws IndexOutOfBoundsException Si el indice esta fuera de los limites del arreglo.
+     */
     @Override
-    public T obtener(int indice) {
+    public T getArreglo(int indice) {
         if (indice < 0 || indice >= longitud) {
-            throw new IndexOutOfBoundsException("Índice fuera de los límites");
+            throw new IndexOutOfBoundsException("Indice fuera de los limites");
         }
         return elementos[indice];
     }
 
+    /**
+     * Verifica si el arreglo unidimensional contiene el elemento especificado.
+     *
+     * @param elemento El elemento a buscar en el arreglo.
+     * @return {@code true} si el arreglo contiene el elemento, {@code false} en caso contrario.
+     */
     @Override
     public boolean contiene(T elemento) {
         for (int i = 0; i < longitud; i++) {
@@ -165,16 +167,31 @@ public class Arreglo<T> implements Coleccion<T> {
         return false;
     }
 
+    /**
+     * Retorna la cantidad de elementos actualmente en el arreglo unidimensional.
+     *
+     * @return El numero de elementos en el arreglo.
+     */
     @Override
     public int longitud() {
         return longitud;
     }
 
+    /**
+     * Verifica si el arreglo unidimensional esta vacio.
+     *
+     * @return {@code true} si el arreglo no contiene elementos, {@code false} en caso contrario.
+     */
     @Override
     public boolean estaVacio() {
         return longitud == 0;
     }
 
+    /**
+     * Devuelve una representacion en forma de cadena del arreglo unidimensional.
+     *
+     * @return Una cadena que representa el contenido del arreglo.
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("[");
@@ -188,11 +205,19 @@ public class Arreglo<T> implements Coleccion<T> {
         return sb.toString();
     }
 
+    /**
+     * Metodo auxiliar para mostrar el contenido de un arreglo 2D.
+     *
+     * @param arreglo El arreglo a mostrar.
+     * @param filas El numero de filas del arreglo.
+     * @param columnas El numero de columnas del arreglo.
+     * @return Una cadena que representa el contenido del arreglo 2D.
+     */
     public String mostrarArreglo2D(Arreglo<Integer> arreglo, int filas, int columnas) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
-                sb.append(arreglo.getArreglo(i, j)).append("\t");
+                sb.append(arreglo.getArreglo(i, j)).append(" ");
             }
             sb.append("\n");
         }
